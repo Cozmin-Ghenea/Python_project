cac5c84b51ef1b923aca2844cb8bf835e7c6ab81
Cozmin <cozmin.ghenea@gmail.com>
2024-09-19 12:03:36 +0300

    Oblig user-ul sa introduca un singur caracter + un caracter neintrodus

--- curs02/curs-for.py
+++ curs02/curs-for.py
@@ -35,29 +35,40 @@
 
 cuvant = 'onomatopee'
 cuvant_de_inlocuit = ""
-
+litere_folosite = "" # vom salva litere folosite deja
 for i in cuvant:
-   if i!=cuvant[0] and i!=cuvant[-1]:
-       cuvant_de_inlocuit += "_"
-   else:
-       cuvant_de_inlocuit += i
+    if i!=cuvant[0] and i!=cuvant[-1]:
+        cuvant_de_inlocuit += "_"
+    else:
+        cuvant_de_inlocuit += i
+        litere_folosite += i
 print(cuvant_de_inlocuit)
-nr_vieti = 3
+nr_vieti = 6
+caracter_cerut=""
 while cuvant_de_inlocuit != cuvant and nr_vieti !=0:
-    caracter_cerut = input("Alege o litera: ")
+    while (len(caracter_cerut) != 1) or (caracter_cerut in litere_folosite):
+        caracter_cerut = input("Alege o litera: ")
+        if(len(caracter_cerut) != 1): # il oprim daca introduce mai multe de un singur caracter
+            print(f"Incearca te rog doar un singur caracter: {caracter_cerut} -> prea multe caractere")
+        if(caracter_cerut in litere_folosite): # il oprim daca caracterul introdus a mai fost folosit odata
+            print(f"Litera {caracter_cerut} a mai fost folosita odata, te rog sa nu folosesti urmatoarele litere : {litere_folosite}")
     print(caracter_cerut)
+    litere_folosite += caracter_cerut # daca litera nu a mai fost folosita o vom introduce intr-un string
     if caracter_cerut in cuvant:
         lista_cuvant_de_inlocuit = list(cuvant_de_inlocuit)
+
         for i,v in enumerate(cuvant):
             if v == caracter_cerut:
              lista_cuvant_de_inlocuit[i] = caracter_cerut
              cuvant_de_inlocuit = "".join(lista_cuvant_de_inlocuit)
+
     else:
         nr_vieti-=1
         if nr_vieti != 0:
             print(f"Mai ai {nr_vieti} vieti")
+    caracter_cerut = ""
     print(cuvant_de_inlocuit)
-if(nr_vieti ==0 ):
+if(nr_vieti ==0):
     print(f"Ai pierdut cuvantul era {cuvant}")
 else:
     print("Ai Castigat")
